e3319c2248268382b4bf8a259bd4d01bdaeced95
Alina Grin <70023302+chebupelkaa@users.noreply.github.com>
2025-01-03 18:17:14 +0300

    Добавьте файлы проекта.

---
+++ VideoApp/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace VideoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.btnInputURL = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.BackColor = System.Drawing.Color.White;
+            this.textBoxURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxURL.Location = new System.Drawing.Point(12, 3);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(860, 30);
+            this.textBoxURL.TabIndex = 0;
+            // 
+            // btnInputURL
+            // 
+            this.btnInputURL.BackColor = System.Drawing.Color.Transparent;
+            this.btnInputURL.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnInputURL.Location = new System.Drawing.Point(878, 3);
+            this.btnInputURL.Name = "btnInputURL";
+            this.btnInputURL.Size = new System.Drawing.Size(93, 33);
+            this.btnInputURL.TabIndex = 1;
+            this.btnInputURL.Text = "Ввод";
+            this.btnInputURL.UseVisualStyleBackColor = false;
+            this.btnInputURL.Click += new System.EventHandler(this.btnInputURL_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Transparent;
+            this.btnClear.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(977, 3);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(115, 33);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1411, 634);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnInputURL);
+            this.Controls.Add(this.textBoxURL);
+            this.Name = "Form1";
+            this.Text = "YouTube";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button btnInputURL;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ VideoApp/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Web;
+using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using CefSharp.WinForms;
+using CefSharp;
+
+namespace VideoApp
+{
+    public partial class Form1 : Form
+    {
+        private ChromiumWebBrowser browser;
+        private readonly string apiKey = "AIzaSyByzPkKv-T5Izf_V7yHTvzGiGGITvAUNWw";
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChromium();
+            this.WindowState = FormWindowState.Maximized;
+ 
+        }
+        private void InitializeChromium()
+        {
+            Cef.Initialize(new CefSettings());
+            browser = new ChromiumWebBrowser();
+            this.Controls.Add(browser);
+            browser.Dock = DockStyle.Fill;
+        }
+        private void btnInputURL_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string url = textBoxURL.Text;
+                string videoId = ExtractVideoId(url);
+
+                if (!string.IsNullOrEmpty(videoId))
+                {
+                    browser.Load($"https://www.youtube.com/embed/{videoId}");
+                }
+                else
+                {
+                    MessageBox.Show("Введите корректную ссылку на YouTube видео.");
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Формат URL некорректен. Пожалуйста, введите правильный URL.");
+            }
+
+        }
+
+        private string ExtractVideoId(string url)
+        {
+            var uri = new Uri(url);
+            var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+            return query["v"] ?? uri.Segments[uri.Segments.Length - 1];
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBoxURL.Clear();
+        }
+    }
+}
